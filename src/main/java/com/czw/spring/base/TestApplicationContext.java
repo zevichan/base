f3bb25e8f9bf74c46b9ddb7193d838a5d003c8f3
zevichen <zevichen@qq.com>
2016-09-09 23:49:34 +0800

    update spring

--- src/main/java/com/czw/spring/base/TestApplicationContext.java
+++ src/main/java/com/czw/spring/base/TestApplicationContext.java
@@ -1,13 +1,21 @@
 package com.czw.spring.base;
 
+import java.util.Properties;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.support.BeanDefinitionDefaults;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.PropertiesBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.czw.spring.base.ioc.Person;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.*;
 
 /**
  * @author ZeviChen
@@ -18,21 +26,58 @@ public class TestApplicationContext {
 	@Test
 	@Ignore
 	public void beanPostProcessor() {
-		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-test/spring-ioc.xml");
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("spring-test/spring-ioc.xml");
+		
 		Person person = ctx.getBean(Person.class);
 		person.sing("Only One - BoA");
+		ctx.close();
+		
+		//在非web应用程序中调用,用于优雅的关闭spring ioc
+		//ctx.registerShutdownHook();
 	}
 
 	/**
 	 * 可以将外部创建的对象放入ioc容器中
 	 */
 	@Test
+	@Ignore
 	public void outsideBeanRegistrater() {
 		BeanDefinitionDefaults bdb = new BeanDefinitionDefaults();
 
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("spring-test/spring-ioc.xml");
 		DefaultListableBeanFactory bf = (DefaultListableBeanFactory) ctx.getBeanFactory();
 		// bf.registerBeanDefinition("", beanDefinition);
+
+	}
+	
+	@Test
+	public void testUnreferencedSingletonWasInstantiated() {
+		KnowsIfInstantiated.clearInstantiationRecord();
+		DefaultListableBeanFactory lbf = new DefaultListableBeanFactory();
+		Properties p = new Properties();
+		p.setProperty("x1.(class)", KnowsIfInstantiated.class.getName());
+		assertTrue("singleton not instantiated", !KnowsIfInstantiated.wasInstantiated());
+		(new PropertiesBeanDefinitionReader(lbf)).registerBeanDefinitions(p);
+		lbf.preInstantiateSingletons();
+		assertTrue("singleton was instantiated", KnowsIfInstantiated.wasInstantiated());
+	}
+	
+	@SuppressWarnings("unused")
+	private static class KnowsIfInstantiated {
+
+		private static boolean instantiated;
+
+		public static void clearInstantiationRecord() {
+			instantiated = false;
+		}
+
+		public static boolean wasInstantiated() {
+			return instantiated;
+		}
+
+		public KnowsIfInstantiated() {
+			instantiated = true;
+		}
 
 	}
 
