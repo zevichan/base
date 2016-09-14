b6599b955a6ac9ddb8c59ccfbbe031350c93dec9
ZeviChan <648350335@qq.com>
2016-09-14 17:19:26 +0800

    udpate

---
+++ src/main/java/com/czw/server/tomcat/TomcatTest.java
@@ -0,0 +1,24 @@
+package com.czw.server.tomcat;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import org.junit.Test;
+
+/**
+ * 
+ * @author ZeviChen
+ * @Date 2016-09-14 17:15:32
+ */
+public class TomcatTest {
+	
+	@Test
+	public void test() throws LifecycleException{
+		Tomcat tomcat = new Tomcat();
+		tomcat.start();
+		tomcat.stop();
+	}
+	
+	
+	
+	
+}
