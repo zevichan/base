fd7bf2ec4cd91f716e05ad8c65a91a21dbb4f0de
Administrator <Administrator@PC201604071839>
2016-09-19 21:15:26 +0800

    updating

---
+++ src/main/java/com/czw/server/jetty/TestHttpClient.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.czw.server.jetty;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author ZeviChen , 2016-09-19 17:40:52
+ */
+public class TestHttpClient {
+	
+	@Test
+	public void test(){
+		// Instantiate HttpClient
+//		HttpClient httpClient = new HttpClient();
+
+		// Configure HttpClient, for example:
+//		httpClient.setFollowRedirects(false);
+//
+//		// Start HttpClient
+//		httpClient.start();
+		
+	}
+}
