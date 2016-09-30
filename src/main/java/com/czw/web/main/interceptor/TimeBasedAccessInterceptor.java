5a0300930e6db005e84fbe3f2dc4c13b63f2639c
ZeviChen <648350335@qq.com>
2016-09-30 17:29:17 +0800

    Update base

---
+++ src/main/java/com/czw/web/main/interceptor/TimeBasedAccessInterceptor.java
@@ -0,0 +1,35 @@
+package com.czw.web.main.interceptor;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+/**
+ * @author ZeviChen ${datetime}
+ */
+public class TimeBasedAccessInterceptor extends HandlerInterceptorAdapter {
+
+    private int openingTime;
+    private int closingTime;
+
+    public void setOpeningTime(int openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public void setClosingTime(int closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+                             Object handler) throws Exception {
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        if (openingTime <= hour && hour < closingTime) {
+            return true;
+        }
+        response.sendRedirect("http://host.com/outsideOfficeHours.html");
+        return false;
+    }
+}
